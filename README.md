a82263b650f1ebf69247531f49c45489262959cf
Horacio Herrera <hi@horacioh.com>
2020-02-27 17:14:12 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Slate plugin `withMarkdownParser`
+
+This Slate plugin allows you to paste markdown content into the editor and convert it to slate nodes.
+
+## [DEMO](https://codesandbox.io/s/slate-reproductions-typxd) (TODO)
+
+## Installation
+
+```shell
+
+yarn add @horacioh/slate-plugin-with-markdown-parser
+# or
+npm install @horacioh/slate-plugin-with-markdown-parser --save
+```
+
+## TODOs
+
+- [ ] remove all `any` types :man_facepalming:
+- [ ] docs
+- [ ] examples
+- [ ] contributing guidelines
+- [ ] tests
+- [ ] CI/CD Github actions
+- [ ] Storybook examples
---
+++ src/withMarkdownParser.ts
@@ -0,0 +1,114 @@
+import { Transforms, Node, Editor } from 'slate';
+import { ReactEditor } from 'slate-react';
+import { jsx } from 'slate-hyperscript';
+import marked from 'marked';
+
+export function withMarkdownParser<T extends Editor>(
+  editor: T
+): Editor & ReactEditor {
+  const e = editor as T & ReactEditor;
+  const { insertData, isInline, isVoid } = e;
+
+  e.isInline = element => {
+    return element.type === 'link' ? true : isInline(element);
+  };
+
+  e.isVoid = element => {
+    return element.type === 'image' ? true : isVoid(element);
+  };
+
+  e.insertData = data => {
+    const content = data.getData('text/plain');
+
+    if (content) {
+      const html = marked(content);
+      const parsed = new DOMParser().parseFromString(html, 'text/html');
+
+      // `filterBreaklines` filters all the breaklines in the pasted document
+      const fragment: Array<Node> = deserialize(parsed.body).filter(
+        filterBreaklines
+      );
+
+      Transforms.insertFragment(editor, fragment);
+      return;
+    }
+
+    insertData(data);
+  };
+
+  return e;
+}
+
+function filterBreaklines(item: any): Boolean {
+  return !item.text;
+}
+
+export const ELEMENT_TAGS: any = {
+  A: (el: any) => ({ type: 'link', url: el.getAttribute('href') }),
+  BLOCKQUOTE: () => ({ type: 'quote' }),
+  H1: () => ({ type: 'heading-one' }),
+  H2: () => ({ type: 'heading-two' }),
+  H3: () => ({ type: 'heading-three' }),
+  H4: () => ({ type: 'heading-four' }),
+  H5: () => ({ type: 'heading-five' }),
+  H6: () => ({ type: 'heading-six' }),
+  IMG: (el: any) => ({ type: 'image', url: el.getAttribute('src') }),
+  LI: () => ({ type: 'list-item' }),
+  OL: () => ({ type: 'numbered-list' }),
+  P: () => ({ type: 'paragraph' }),
+  PRE: () => ({ type: 'code' }),
+  UL: () => ({ type: 'bulleted-list' }),
+};
+
+// COMPAT: `B` is omitted here because Google Docs uses `<b>` in weird ways.
+export const TEXT_TAGS: any = {
+  CODE: () => ({ code: true }),
+  DEL: () => ({ strikethrough: true }),
+  EM: () => ({ italic: true }),
+  I: () => ({ italic: true }),
+  S: () => ({ strikethrough: true }),
+  STRONG: () => ({ bold: true }),
+  U: () => ({ underline: true }),
+};
+
+export function deserialize(el: any) {
+  if (el.nodeType === 3) {
+    return el.textContent;
+  } else if (el.nodeType !== 1) {
+    return null;
+  } else if (el.nodeName === 'BR') {
+    return '\n';
+  }
+
+  const { nodeName } = el;
+  let parent = el;
+
+  if (
+    nodeName === 'PRE' &&
+    el.childNodes[0] &&
+    el.childNodes[0].nodeName === 'CODE'
+  ) {
+    parent = el.childNodes[0];
+  }
+
+  // TODO: fix types
+  const children: Array<any> = Array.from(parent.childNodes)
+    .map(deserialize)
+    .flat();
+
+  if (el.nodeName === 'BODY') {
+    return jsx('fragment', {}, children);
+  }
+
+  if (ELEMENT_TAGS[nodeName]) {
+    const attrs = ELEMENT_TAGS[nodeName](el);
+    return jsx('element', attrs, children);
+  }
+
+  if (TEXT_TAGS[nodeName]) {
+    const attrs = TEXT_TAGS[nodeName](el);
+    return children.map(child => jsx('text', attrs, child));
+  }
+
+  return children;
+}
